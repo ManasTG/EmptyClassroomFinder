2ceb2c69c1c1024c25c5c4764b9270e5e5e4b555
Manas Madhukar <manas.jilal@gmail.com>
2026-09-11 16:50:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,7 @@
-import os
-import psycopg2
 from flask import Flask, jsonify, request, send_from_directory
 from src.emptyClassroom import emptyClassroomFinder
 
 app = Flask(__name__, static_folder="frontend", static_url_path="")
-
-def init_db():
-    conn = psycopg2.connect(os.environ["DATABASE_URL"])
-    cur = conn.cursor()
-
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS timetable (
-            id SERIAL PRIMARY KEY,
-            section TEXT NOT NULL,
-            classroom TEXT,
-            day TEXT NOT NULL,
-            period INTEGER NOT NULL
-        )
-    """)
-
-    conn.commit()
-    cur.close()
-    conn.close()
 
 @app.before_request
 def log_request():
@@ -36,6 +16,7 @@ def log_response(response):
 def home():
     return send_from_directory('frontend', 'index.html')
 
+
 @app.route('/find')
 def find_room():
     day = request.args.get("day")
@@ -45,9 +26,5 @@ def find_room():
 
     return jsonify(rooms)
 
-
-init_db()
-
 if __name__ == "__main__":
     app.run()
-
